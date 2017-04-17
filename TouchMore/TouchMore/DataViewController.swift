e97a1c985fdcbc83460625820b1267640e33cc80
戚培源 <peiyuanqi@Peiyuans-MBP.local>
2017-04-16 20:25:40 -0400

    init

---
+++ TouchMore/TouchMore/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  TouchMore
+//
+//  Created by 戚培源 on 16/04/2017.
+//  Copyright © 2017 戚培源. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
