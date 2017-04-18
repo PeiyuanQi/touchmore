9c4311e5f7629fa76e0bf74f7b3631442fe694d6
戚培源 <peiyuanqi@Peiyuans-MBP.local>
2017-04-18 10:31:14 -0400

    delete xcode view

--- TouchMore/TouchMore/MCTableViewController.swift
+++ TouchMore/TouchMore/MCTableViewController.swift
@@ -34,7 +34,7 @@ class MCTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 4
+        return 3
     }
 
     /*
