298e80abf88051baf4a774501e6810abaf3c77c6
戚培源 <qipeiyuan@outlook.com>
2017-04-28 14:20:01 -0400

    fix problem of sandbox

--- TouchMore/TouchMore/settingViewController.swift
+++ TouchMore/TouchMore/settingViewController.swift
@@ -72,7 +72,7 @@ class settingViewController: UITableViewController, UIPickerViewDelegate,UIPicke
     let keys = [
         ["","Shift ", "Control ", "Option "],
         ["","Shift ","Option ", "Command "],
-        ["","A","B","C","D","E","F","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","G","H","I","J","K","L","M","N"]
+        ["","A","B","C","D","E","F","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
     ]
     
     //picker functions
